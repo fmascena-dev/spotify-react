d228f4e896f370f4e2650d9421bd1c2817caad01
felipe mascena <felipe.mascena.dev@gmail.com>
2025-02-04 20:28:23 -0300

    finalizando o Footer

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Footer from './components/Footer/Footer'
 import Header from './components/Header/Header'
 import Main from './components/Main/Main'
 import Sidebar from './components/Sidebar/Sidebar'
@@ -11,6 +12,7 @@ export default function App() {
         <Header />
         <Main />
         <Sidebar />
+        <Footer />
       </div>
     </>
   )
